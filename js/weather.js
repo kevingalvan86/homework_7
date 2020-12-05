653f5c5fe110b3cd3c03f096af3cda207e105694
Kevin Felipe Galvan <kevinfgalvan@gmail.com>
2020-12-04 19:17:36 -0500

    image

--- js/weather.js
+++ js/weather.js
@@ -37,26 +37,24 @@ function gettingJSON(){
     if (loc_input != ""){
         location = loc_input;
     }
-    console.log( location);
-
     var check_radio = document.querySelector('input[name="temp"]:checked');
     if (check_radio != null) {
         format = check_radio.value;
     }
-    console.log(format);
     query = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=" + format + "&appid=ff50bde9df369a377f0b0ff4e5987cd4";
     document.getElementById("forecast").style.display = "block"
-
     $.getJSON(query,function(json){
         //Use returned json to update the values of the three 
         //elements in HTML.  
         //I would print the JSON to the console
         // Your code here.
-        //console.log(json)
         console.log(json.main.temp);
         console.log(json.name);
         document.getElementById("loc").innerText = json.name;
         document.getElementById("temp").innerText = json.main.temp + " with " + json.weather[0].description;
-
+        let url; 
+        url = "http://openweathermap.org/img/wn/" + json.weather[0].icon + ".png";
+        document.getElementById("tempImg").src = url;
+        document.getElementById("tempImg").alt = json.weather[0].description;
     });
 }
